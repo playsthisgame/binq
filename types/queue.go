4c3d2d0c195b4efc6cc42924ef5aabf492f786cd
Playsthisgame <plysthisgame@gmail.com>
2024-10-23 21:25:56 -0400

    adding to the queue

--- types/queue.go
+++ types/queue.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Queue struct {
 	gorm.Model
 	Name          string `gorm:"index" json:"name"`
-	maxPartitions string `json:"maxPartitions"`
+	MaxPartitions string `json:"maxPartitions"`
 }
